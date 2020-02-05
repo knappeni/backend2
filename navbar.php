d041562300521b9201b0034e857c6d4dfd23b3e6
knappeni <knappeni@arcada.fi>
2020-02-05 16:43:36 +0200

    navbar + list fixes

--- navbar.php
+++ navbar.php
@@ -5,29 +5,29 @@
             if ($_SESSION['roll'] == 'user') {
                 print("
                 <a href='index.php'>Home</a>
-                <a href='lista.php'>List</a>
+                <a href='lista.php'>Användare</a>
                 <a href='annonser.php'>Annonser</a>
-                <a href='matain.php'>Mata in</a>
-                <a href='ajax.php'>Ajax</a>
+                <a href='matain.php'>Ny annons</a>
+                <a href='ajax.php'>Sök annonser</a>
                 <a href='logout.php'>Logga ut</a>
                 <div class='loggedDiv'><a href='profil.php?user=".$_SESSION['username']."'>Loggad in som: ".$_SESSION['username']."</a></div>");
             } elseif ($_SESSION['roll'] == 'editor') {
                 print("
                 <a href='index.php'>Home</a>
-                <a href='lista.php'>List</a>
+                <a href='lista.php'>Användare</a>
                 <a href='annonser.php'>Annonser</a>
-                <a href='matain.php'>Mata in</a>
-                <a href='ajax.php'>Ajax</a>
+                <a href='matain.php'>Ny annons</a>
+                <a href='ajax.php'>Sök annonser</a>
                 <a href='logout.php'>Logga ut</a>
                 <div class='loggedDiv'><a href='profil.php?user=".$_SESSION['username']."'>Loggad in som: ".$_SESSION['username']."</a></div>");
             } elseif ($_SESSION['roll'] == 'admin') {
                 print("
                 <a href='index.php'>Home</a>
-                <a href='lista.php'>List</a>
+                <a href='lista.php'>Användare</a>
                 <a href='annonser.php'>Annonser</a>
-                <a href='matain.php'>Mata in</a>
+                <a href='matain.php'>Ny annons</a>
                 <a href='radera.php'>Radera</a>
-                <a href='ajax.php'>Ajax</a>
+                <a href='ajax.php'>Sök annonser</a>
                 <a href='logout.php'>Logga ut</a>
                 <div class='loggedDiv'><a href='profil.php?user=".$_SESSION['username']."'>Loggad in som: ".$_SESSION['username']."</a></div>");
             }
