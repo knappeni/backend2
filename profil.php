0a10912e9c09003f8ce084c6c5ddfd327b544259
mutkajoakim <33824586+mutkajoa@users.noreply.github.com>
2020-02-05 17:16:54 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -14,10 +14,72 @@ include("handyfunctions.php");
 <h1>Din Profil</h1>
 <?php 
 include("navbar.php"); 
+if(isset($_SESSION['username'])){
+    
+    if(!isset($_GET['user']) || $_SESSION['username'] == $_GET['user'] ){
+        
+        $conn = create_conn();
+        // Check connection
+        if ($conn->connect_error) {
+            die("<p>Connection failed: " . $conn->connect_error."</p>");
+        } 
+    
+        // Uppkoppling ok, kör SQL kommandon härefter:
+        $sql = "SELECT * FROM users WHERE namn='".$_SESSION['username']."';";
+        
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $sql1 = "SELECT * FROM loppis WHERE saljare='".$row['namn']."';";
+                $rows = $conn->query($sql1);
+                print("<p>Användarnamn: ".$row['namn']."<br>
+                        Email: ".$row['epost']."<br>
+                        Din roll är: ".$row['roll']."<br>
+                        Registrerad sedan: ".$row['datum']."<br>
+                        Antal annonser: ".$rows->num_rows."
+                        </p>");
+            };
+            $conn->close();
+    }}
+    else {
+        $conn = create_conn();
+        // Check connection
+        if ($conn->connect_error) {
+            die("<p>Connection failed: " . $conn->connect_error."</p>");
+        } 
+           
+            // Uppkoppling ok, kör SQL kommandon härefter:
+         $sql = "SELECT * FROM users WHERE namn='". $_GET['user'] ."';";
+        
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                 while ($row = $result->fetch_assoc()) {
+                    $sql1 = "SELECT * FROM loppis WHERE saljare='".$row['namn']."';";
+                    $rows = $conn->query($sql1);
+                    print("<p>Användarnamn: ".$row['namn']."<br>
+                        Email: ".$row['epost']."<br>
+                        Användaren ".$row['namn']."s roll är: ".$row['roll']."<br>
+                        Registrerad sedan: ".$row['datum']."<br>
+                        Antal annonser: ".$rows->num_rows."
+                        </p>");
+                };
+            }
+             else {
+                print("<p>Ingen profil hittades</p>");
+                }
+                // Kom ihåg att stänga databasuppkopplingen
+                $conn->close();
+        }
     //if session user == GET user (eller om GET user är tom)
     //visa din egen profil med möjlighet att editera din info
     //elseif session user != GET user
     //visa GET user profil, men ingen möjlighet för att editera
+    }
+    else{
+        print("<p>Du måste vara inloggad för att se profiler</p>");
+    }
 ?>
 </body>
 </html>
