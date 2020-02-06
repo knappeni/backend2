5b11613f0b17bf11b346185dec7e02369f7105c4
knappeni <knappeni@arcada.fi>
2020-02-06 17:55:56 +0200

    murrrrr

--- delete.php
+++ delete.php
@@ -2,12 +2,14 @@
 // Sessionshantering
 include("handyfunctions.php");
 $conn = create_conn();
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-print("HEJEJEJE");
+#if ($conn->connect_error) {
+#    die("Connection failed: " . $conn->connect_error);
+#}
+$surr = ['hidden'];
+print($surr." hidden");
 $deleteDB = "DELETE FROM loppis WHERE id = '".$_GET['delID']."'";
+print($deleteDB);
 $conn->query($deleteDB);
-header ("Location: test.php");
-$conn->close();
+#header ("Location: test.php");
+#$conn->close();
 ?>
--- test.php
+++ test.php
@@ -11,12 +11,12 @@ include("smallnavbar.php");
     <title>Annonser</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <script language="JavaScript" type="text/javascript">
 function delannons(id) {
     if (confirm("Är du säker på att du vill radera annons '" + id + "'")) {
         window.location.href='delete.php?delID=' +id+'';
-        return true;
     }
 }
 </script>
@@ -45,14 +45,18 @@ if (isset($_POST['update'])) {
 $sql = "SELECT * FROM loppis";
 $result = $conn->query($sql);
 echo "<table border=1>
+<thead>
 <tr>
 <th>ID</th>
 <th>Rubrik</th>
 <th>Beskrivning</th>
 <th>Pris €</th>
 <th>Uppladdad</th>
-</tr>";
+</tr>
+</thead>";
 while ($row = $result->fetch_assoc()) {
+    $id = $row['id'];
+    echo "<tbody>";
     echo "<form action=test.php method=post>";
     echo "<tr>";
     echo "<td>".$row['id']."</td>";
@@ -61,15 +65,53 @@ while ($row = $result->fetch_assoc()) {
     echo "<td>".$row['pris']." €</td>";
     echo "<td>".date("d.m.Y H:i:s", strtotime($row['datum']))."</td>";
     echo "<td>"."<input type=hidden name=hidden value=".$row['id'].">"."</td>";
-    echo "<td>"."<input type=submit name=update value=Uppdatera>"."</td>";
-    echo "<td>"."<input type=submit name=delete value=Radera onclick=delannons(".$row['id'].")>"."</td>";
+    #echo "<td>"."<input type=submit name=update value=Uppdatera>"."</td>";
+    #echo "<td>"."<input type=submit name=delete value=Radera onclick=delannons(".$row['id'].")>"."</td>";
+    echo "<td>"."<button class='delete btn btn-danger' id='del_<?= $id ?>' data-id='<?= $id ?>' >Delete</button>"."</td>";
     echo "</tr>";
     echo "</form>";
+    echo "</tbody>";
 }
-
 echo "</table>";
-$conn->close();
+#$conn->close();
 ?>
+<script type="text/javascript">
+$(document).ready(function(){
+// Delete 
+$('.delete').click(function(){
+  var el = this;
+  // Delete id
+  var deleteid = $(this).data('id');
+  // Confirm box
+  confirm("Do you really want to delete record!?", function(result) {
+    alert("hiii");
+     if(result){
+        alert("hiii");
+       // AJAX Request
+       $.ajax({
+         url: 'ajaxfile.php',
+         type: 'POST',
+         data: { id:deleteid },
+         success: function(response){
+           // Removing row from HTML Table
+           if(response == 1){
+               alert("hiii");
+                $(el).closest('tr').css('background','tomato');
+                $(el).closest('tr').fadeOut(800,function(){
+                $(this).remove();
+            });
+            }else{
+                alert('Record not deleted.');
+            }
+         }
+       });
+     } 
+  });
+  alert("hiii111");
+});
+});
+</script>
+<?php $conn->close(); ?>
 </section>
 </body>
 </html>
